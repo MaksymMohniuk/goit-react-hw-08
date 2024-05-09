a5efe951b099d7a9fde62106ab89e685e6a8e716
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-05-09 23:30:57 +0300

    contact-slice-corrections

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -1,5 +1,6 @@
 import { createSlice, isAnyOf } from "@reduxjs/toolkit";
 import { apiAddContact, apiDeleteContact, apiGetContacts } from "./operations";
+import { apiLogout } from "../auth/operations";
 
 const INITIAL_STATE = {
   contacts: null,
@@ -19,16 +20,23 @@ const contactsSlice = createSlice({
       .addCase(apiGetContacts.fulfilled, (state, action) => {
         state.loading = false;
         state.contacts = action.payload;
+        state.error = null;
       })
       .addCase(apiAddContact.fulfilled, (state, action) => {
         state.loading = false;
         state.contacts.push(action.payload);
+        state.error = null;
       })
       .addCase(apiDeleteContact.fulfilled, (state, action) => {
         state.loading = false;
         state.contacts = state.contacts.filter(
           (item) => item.id !== action.payload.id
         );
+        state.error = null;
+      })
+      .addCase(apiLogout.fulfilled, (state) => {
+        state.contacts = null;
+        state.error = null;
       })
       .addMatcher(
         isAnyOf(
